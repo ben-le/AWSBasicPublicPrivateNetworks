de0553bb455ca7afeed9ef6d382b594abd81cc57
Ubuntu <ubuntu@ip-172-31-17-85.us-west-2.compute.internal>
2021-07-22 17:57:26 +0000

    privateEC2AccessInternet

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable region {
+    default="us-west-2"
+}
+
+variable profile {
+    default="personal"
+}
+
+variable instance-ami {
+    default="ami-0088ab5bd1db84ada"
+}
+
+#variable key_path {
+#    default = "keys/mykeypair.pub"
+#}
